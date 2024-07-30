7ec32d353ba12e050d8dc36b347fe884e849bbfe
Iqra Akhtar <iqra01953@gmail.com>
2024-07-30 20:51:25 +0500

    Time: 30 ms (96.90%), Space: 16.5 MB (44.10%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,10 +1,27 @@
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        for i in range(0, len(nums1)):
-            if nums1[i]==0 and len(nums2) > 0:
-                nums1[i]= nums2.pop(0)
-        nums1.sort()
-        return nums1
-        
-        
-        
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n =int) -> None:
+            last = m + n -1
+            while m>0 and n> 0:
+                if nums1[m-1] > nums2[n-1]:
+                    nums1[last] = nums1[m-1]
+                    m -=1
+                else:
+                    nums1[last] = nums2[n-1]
+                    n -=1
+                last -=1 
+            while n>0:
+                nums1[last] = nums2[n-1]
+                n , last = n-1 , last -1
+
+
+# class Solution:
+#     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+#         for i in range (0, len(nums1)):
+#             if nums1[i] == 0 and len(nums2)>0:
+#                 nums1[i] = nums2.pop(0)
+#                 print(nums1)
+#         nums1.sort()
+#         return nums1
+     
+#         nums1[m:] = nums2
+#         nums1.sort()
