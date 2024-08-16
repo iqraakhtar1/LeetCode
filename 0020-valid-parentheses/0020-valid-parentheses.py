20b9342c2be1d062982bb36d3dd626d39d9a4595
Iqra Akhtar <iqra01953@gmail.com>
2024-08-16 18:07:09 +0500

    Time: 33 ms (73.76%), Space: 16.6 MB (47.21%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,18 +1,32 @@
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+#         stack = []
+#         closeTo = {")":"(", "}":"{","]":"["}
+#         for c in s:
+#             if c in closeTo:
+#                 if stack and stack[-1] == closeTo[c]:
+#                     stack.pop()
+#                 else:
+#                     return False
+#             else:
+#                 stack.append(c)
+#         return True if not stack else False
+       
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = []
-        closeTo = {")":"(", "}":"{","]":"["}
+        st=[]
         for c in s:
-            if c in closeTo:
-                if stack and stack[-1] == closeTo[c]:
-                    stack.pop()
-                else:
-                    return False
+            if c in '({[':
+                st.append(c)
             else:
-                stack.append(c)
-        return True if not stack else False
-       
-                
+                if not st or \
+                c==')' and st[-1]!='(' or \
+                c=='}' and st[-1]!='{' or \
+                c==']' and st[-1]!='[':
+                    return False
+                else:
+                    st.pop()
+        return not st                
                     
                 
     
