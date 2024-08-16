b304b5779c7036ca505ee96f04e6e546e2d69df5
Iqra Akhtar <iqra01953@gmail.com>
2024-08-16 17:30:08 +0500

    Time: 32 ms (77.02%), Space: 16.3 MB (93.36%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,8 +1,15 @@
 class Solution:
     def numJewelsInStones(self, jw: str, st: str) -> int:
+        # count = 0
+        # for i in range(len(jw)):
+        #     for j in range(len(st)):
+        #         if jw[i] is st[j]:
+        #             count += 1
+        # return count
+        
         count = 0
-        for i in range(len(jw)):
-            for j in range(len(st)):
-                if jw[i] is st[j]:
-                    count += 1
+        jewll = set(jw)
+        for i in st:
+            if i in jewll:
+                count+=1
         return count
