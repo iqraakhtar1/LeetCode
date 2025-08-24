f7494a42f32a12689e0456bf820e421ddb23f125
Iqra Akhtar <iqra01953@gmail.com>
2025-08-24 17:07:22 +0500

    Time: 68 ms (6.2%), Space: 13.8 MB (100%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.py
@@ -0,0 +1,27 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:        
+        result = ""
+        
+        aLength = len(a) - 1
+        bLength = len(b) - 1
+        
+        carry = 0
+        
+        while aLength >= 0 or bLength >= 0:
+            
+            totalSum = carry
+            if aLength >= 0:
+                totalSum += int(a[aLength])
+                aLength -= 1
+            if bLength >= 0:
+                totalSum += int(b[bLength])
+                bLength -= 1
+            
+            result = str(totalSum % 2) + result
+            
+            carry = totalSum // 2
+        
+        if carry > 0:
+            result = str(1) + result
+        return result
+
