a08405705a3480f24e096ca758e29aa97cc675b1
Iqra Akhtar <iqra01953@gmail.com>
2025-08-24 16:50:07 +0500

    Time: 50 ms (84.29%), Space: 28.7 MB (69.72%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -17,30 +17,13 @@
 #   - Worst Case: O(n^2)
 # Extra Space Complexity: O(1)
 class Solution:
-    def partition(self, nums: List[int], left: int, right: int) -> int:
-        pivot, fill = nums[right], left
-
-        for i in range(left, right):
-            if nums[i] <= pivot:
-                nums[fill], nums[i] = nums[i], nums[fill]
-                fill += 1
-
-        nums[fill], nums[right] = nums[right], nums[fill]
-
-        return fill
-
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        k = len(nums) - k
-        left, right = 0, len(nums) - 1
-
-        while left < right:
-            pivot = self.partition(nums, left, right)
-
-            if pivot < k:
-                left = pivot + 1
-            elif pivot > k:
-                right = pivot - 1
-            else:
-                break
-
-        return nums[k]
+        heap = nums[:k]
+        heapq.heapify(heap)
+        
+        for num in nums[k:]:
+            if num > heap[0]:
+                heapq.heappop(heap)
+                heapq.heappush(heap, num)
+        
+        return heap[0]
